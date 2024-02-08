8792b366cbb3d3fa3d014af236a8e9668a4227e9
Anonymsubb <159455755+Anonymsubb@users.noreply.github.com>
2024-02-08 17:16:34 +0200

    Update max_queue.py

--- irma/max_queue.py
+++ irma/max_queue.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# priorityq: An object-oriented priority queue with updatable priorities.
-#
-# Copyright 2018 Edward L. Platt
-#
-# This software is released under multiple licenses. See the LICENSE file for
-# more information.
-#
-# Authors:
-#   Edward L. Platt <ed@elplatt.com>
+
 """Priority queue class with updatable priorities.
 """
 
